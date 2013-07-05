74d0930e3c917099941cd0a2b49fd00a0c539126
Brad Wilcox <brad_wilcox@me.com>
2013-07-04 21:48:55 -0400

    add picture_controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Photogur::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  get 'pictures' => 'pictures#index'
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
